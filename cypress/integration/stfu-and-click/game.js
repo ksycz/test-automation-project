f921893c5334d2f53f40f007700bc8de48280739
ksycz <katarzynasycz@gmail.com>
2020-05-20 17:13:09 +0200

    added new tests

--- cypress/integration/stfu-and-click/game.js
+++ cypress/integration/stfu-and-click/game.js
@@ -12,4 +12,26 @@ describe('Testing the game screen', function() {
         .get(selectors.inputLinkForTeam)
         .should('have.value', context.linkForTeam)
     });
+
+    it('2.4 - The number of clicks before clicking the Click! button should be lower than the number after click', function () {
+        cy.get(selectors.activeTeamNameRowClicks).invoke('text').then((initialNr) => {
+            cy.get(selectors.clickActionBtn).click()
+            cy.get(selectors.activeTeamNameRowClicks).invoke('text').should((updatedNr) => {
+            expect(parseInt(initialNr)).to.be.lessThan(parseInt(updatedNr))
+            })
+        })
+    })
+
+    it.skip('2.6 - The number of clicks should not change after pressing the Enter key on the keyboard', function () {
+        cy.get(selectors.activeTeamNameRowClicks).invoke('text').then((initialNr) => {
+            for (let i = 0; i < 10; i++) {
+                cy.get(selectors.clickActionBtn).trigger('mouseover').trigger('keydown', { keycode: 13, release: false })
+                cy.get(selectors.clickActionBtn).trigger('mouseover').trigger('keypress', { keycode: 13, release: false })
+                cy.wait(50)
+              }
+            cy.get(selectors.activeTeamNameRowClicks).invoke('text').should((updatedNr) => {
+            expect(parseInt(initialNr)).to.be.eq(parseInt(updatedNr))
+            })
+        })
+    })
 });
